0a0d099306a773362aca30cf7679b8768fd48d96
estefania repetti <repetti.stefania@gmail.com>
2022-05-25 17:23:32 -0300

    nav and categories

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import {Link, useNavigate} from "react-router-dom"
 import AddButton from '../AddButton/AddButton'  
 
-const Item = ({producto}) => {
+export default function Item ({item}){
+const navigate = useNavigate()
 
   return (
       
     <div className="card" style={{width:'18rem', margin:'.5rem'}}>
-    <img src={producto.img} className="card-img-top" alt={producto.name}/>
+    <img src={item.img} className="card-img-top" alt={item.name}/>
     <div className="card-body">
-        <p className="card-text">{producto.name}</p>
-        <p className="card-text">{producto.description}</p>
+        <p className="card-text">{item.name}</p>
+        <p className="card-text">{item.description}</p>
     </div>
     
-    
+
     <AddButton />
 </div>
   )
 }
 
-export default Item
